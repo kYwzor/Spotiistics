4302b37ecf7202c229b1d8ae8edd5b211bb2b7c7
Leonardomav <leomavieira97@gmail.com>
2019-12-28 16:12:17 +0000

    completed playlists and minor fixs

--- app/src/main/java/com/example/spotiistics/ArtistsInfoFragment.java
+++ app/src/main/java/com/example/spotiistics/ArtistsInfoFragment.java
@@ -1,15 +1,20 @@
 package com.example.spotiistics;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.bumptech.glide.Glide;
 
@@ -52,9 +57,37 @@ public class ArtistsInfoFragment extends ItemFragment {
                 LinearLayout base = rootview.findViewById(R.id.lista_albums);
 
                 itemClickListener itemClickListener = new itemClickListener();
+
                 for(Album a : ap.items) {
-                    LinearLayout ll = aa.createLinearLayout(a.name, a.id);
+                    LinearLayout ll = new LinearLayout(aa.getApplicationContext());
                     ll.setOnClickListener(itemClickListener);
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    lp.setMargins(20, 0, 10, 50);
+                    ll.setLayoutParams(lp);
+                    ll.setTag(R.id.ID, a.id);
+
+
+                    ImageView iv = new ImageView(aa.getApplicationContext());
+                    iv.setImageDrawable(getResources().getDrawable(R.drawable.noalbum));    //placeholder
+                    ll.addView(iv);
+                    iv.getLayoutParams().height = (int) getResources().getDimension(R.dimen.imageview_thumbnail_size);
+                    iv.getLayoutParams().width = (int) getResources().getDimension(R.dimen.imageview_thumbnail_size);
+
+
+                    TextView tv = new TextView(aa.getApplicationContext());
+                    tv.setEllipsize(TextUtils.TruncateAt.END);
+                    tv.setMaxLines(1);
+                    tv.setText(a.name);
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params.setMargins(15,0,0,0);
+                    tv.setLayoutParams(params);
+                    tv.setTextColor(Color.WHITE);
+
+                    Typeface tf = ResourcesCompat.getFont(aa.getApplicationContext(), R.font.roboto_light);
+                    tv.setTypeface(tf);
+                    tv.setPadding(0,10, 0,0);
+                    ll.addView(tv);
+
                     base.addView(ll);
                     if (a.images.size() != 0){
                         // new DownloadImageTask((ImageView) ll.getChildAt(0)).execute(a.images.get(0).url);
--- app/src/main/java/com/example/spotiistics/PlaylistInfoFragment.java
+++ app/src/main/java/com/example/spotiistics/PlaylistInfoFragment.java
@@ -1,17 +1,31 @@
 package com.example.spotiistics;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.bumptech.glide.Glide;
 
 import java.lang.ref.WeakReference;
 
+import kaaes.spotify.webapi.android.SpotifyCallback;
+import kaaes.spotify.webapi.android.SpotifyError;
+import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.Pager;
 import kaaes.spotify.webapi.android.models.PlaylistTrack;
+import retrofit.client.Response;
 
 public class PlaylistInfoFragment extends ItemFragment {
     private WeakReference<PlaylistActivity> activityReference;
@@ -33,21 +47,77 @@ public class PlaylistInfoFragment extends ItemFragment {
     public void updateData() {
         final PlaylistActivity pa = activityReference.get();
         if (pa==null) return;
+        TextView n_track = rootview.findViewById(R.id.playlist_n_track);
+        n_track.setText(pa.playlist.tracks.total + " tracks");     //TODO: Hardcoded text);
+
         LinearLayout base = rootview.findViewById(R.id.lista_tracks);
 
-        itemClickListener itemClickListener = new itemClickListener();
-        for(PlaylistTrack track : pa.playlist.tracks.items) {
-            TextView tv = pa.createTextView(track.track.name);
-            tv.setOnClickListener(itemClickListener);
-            tv.setTag(track.track.id);
-            base.addView(tv);
+
+        for(final PlaylistTrack track : pa.playlist.tracks.items) {
+            BaseActivity.spotify.getAlbum(track.track.album.id, new SpotifyCallback<Album>() {
+                @Override
+                public void success(Album a, Response response) {
+                    LinearLayout base = rootview.findViewById(R.id.lista_tracks);
+
+                    itemClickListener itemClickListener = new itemClickListener();
+
+                    LinearLayout ll = new LinearLayout(pa.getApplicationContext());
+                    ll.setOnClickListener(itemClickListener);
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    lp.setMargins(20, 0, 10, 50);
+                    ll.setLayoutParams(lp);
+                    ll.setTag(R.id.ID, track.track.id);
+
+
+                    ImageView iv = new ImageView(pa.getApplicationContext());
+                    iv.setImageDrawable(getResources().getDrawable(R.drawable.noalbum));    //placeholder
+                    ll.addView(iv);
+                    iv.getLayoutParams().height = (int) getResources().getDimension(R.dimen.imageview_thumbnail_size);
+                    iv.getLayoutParams().width = (int) getResources().getDimension(R.dimen.imageview_thumbnail_size);
+
+
+                    TextView tv = new TextView(pa.getApplicationContext());
+                    tv.setEllipsize(TextUtils.TruncateAt.END);
+                    tv.setMaxLines(1);
+                    tv.setText(track.track.name);
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params.setMargins(15,0,0,0);
+                    tv.setLayoutParams(params);
+                    tv.setTextColor(Color.WHITE);
+
+                    Typeface tf = ResourcesCompat.getFont(pa.getApplicationContext(), R.font.roboto_light);
+                    tv.setTypeface(tf);
+                    tv.setPadding(0,10, 0,0);
+                    ll.addView(tv);
+
+                    base.addView(ll);
+                    if (a.images.size() != 0){
+                        // new DownloadImageTask((ImageView) ll.getChildAt(0)).execute(a.images.get(0).url);
+                        Glide
+                                .with(pa)
+                                .load(a.images.get(0).url)
+                                .placeholder(R.drawable.noalbum)
+                                .into((ImageView) ll.getChildAt(0));
+                    }
+
+
+
+                }
+
+                @Override
+                public void failure(SpotifyError error) {
+                    Toast.makeText(pa, "Error loading", Toast.LENGTH_LONG).show();
+                }
+            });
         }
+
+
     }
 
     public class itemClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            activityReference.get().changeActivity(TrackActivity.class, (String) v.getTag());
+            activityReference.get().changeActivity(TrackActivity.class, (String) v.getTag(R.id.ID));
         }
     }
 }
